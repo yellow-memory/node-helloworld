3458ee9c23ac683566190b9f23b1172abc21094e
shinystarlight00 <charles.dev.0811@gmail.com>
2024-10-22 12:39:11 -0400

    fix: ready for deployment

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 
 var routes = require("./routes/routes.js")(app);
 
-app.listen(3000, function () {
-  console.log("Listening on port 3000...");
-});
+// app.listen(3000, function () {
+//   console.log("Listening on port 3000...");
+// });
+
+module.exports = app;

